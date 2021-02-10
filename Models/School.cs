ab557acb259e1acc182a43319cb9519c27494056
Siyavash <feyzisiyavash@gmail.com>
2021-02-11 00:33:05 +0330

    init School Entity

--- Models/School.cs
+++ Models/School.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,21 @@ namespace Education_Api_L1.Models
 {
     public class School
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Level { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        [MinLength(11)]
+        public string  PhoneNumber { get; set; }
+
     }
 }
