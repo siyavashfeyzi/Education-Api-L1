8a4fac049b857d21d36f447934c509c9caf0e833
Siyavash <feyzisiyavash@gmail.com>
2021-02-11 00:50:10 +0330

    init StudentReadDto

--- Dtos/StudentDto/StudentReadDto.cs
+++ Dtos/StudentDto/StudentReadDto.cs
@@ -7,5 +7,14 @@ namespace Education_Api_L1.Dtos.StudentDto
 {
     public class StudentReadDto
     {
+        public int id { get; set; }
+        
+        public string name { get; set; }
+
+        public string family { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string Year_of_Birth { get; set; }
     }
 }
