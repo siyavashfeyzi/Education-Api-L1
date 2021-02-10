a8f7934f40c8ffdda3b4163b617fac4be34f6667
Siyavash <feyzisiyavash@gmail.com>
2021-02-11 01:39:04 +0330

    Init StudentCreateDto

--- Dtos/StudentDto/StudentCreateDto.cs
+++ Dtos/StudentDto/StudentCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace Education_Api_L1.Dtos.StudentDto
 {
     public class StudentCreateDto
     {
+        [Required]
+        [MaxLength(300)]
+        public string name { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string family { get; set; }
+
+        [MaxLength(11)]
+        [MinLength(11)]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        public string Year_of_Birth { get; set; }
     }
 }
