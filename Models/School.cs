0be16a68e91a85fde8e51c04c11d65adf8d42414
Siyavash <feyzisiyavash@gmail.com>
2021-02-10 19:01:30 +0330

    Create School Entity

---
+++ Models/School.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Models
+{
+    public class School
+    {
+    }
+}
