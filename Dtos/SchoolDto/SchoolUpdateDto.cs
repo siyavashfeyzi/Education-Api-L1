29b4ceb2a62c637f874e2607a435a597f6eee696
Siyavash <feyzisiyavash@gmail.com>
2021-02-12 22:52:45 +0330

    Create and Init SchoolUpdateDto

---
+++ Dtos/SchoolDto/SchoolUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Dtos.SchoolDto
+{
+    public class SchoolUpdateDto
+    {
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Level { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        [MinLength(11)]
+        public string PhoneNumber { get; set; }
+    }
+}
