22e65e579bfe45713b7d31320584eb2536de12bf
Siyavash <feyzisiyavash@gmail.com>
2021-02-12 23:09:40 +0330

    Create And Init Profile.cs for Student

---
+++ Profiles/StudentProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Education_Api_L1.Dtos.StudentDto;
+using Education_Api_L1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Profiles
+{
+    public class StudentProfile : Profile
+    {
+        public StudentProfile()
+        {
+            //Source => Target
+            CreateMap<Student, StudentReadDto>();
+            CreateMap<StudentCreateDto, Student>();
+            CreateMap<StudentUpdateDto, Student>();
+            CreateMap<Student, StudentUpdateDto>();
+        }
+    }
+}
