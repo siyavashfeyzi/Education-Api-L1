b31af5538394519e029ca3901172e732b3bf7679
Siyavash <feyzisiyavash@gmail.com>
2021-02-12 23:16:56 +0330

    Create and Init School Proflie

---
+++ Profiles/SchoolProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Education_Api_L1.Dtos.SchoolDto;
+using Education_Api_L1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Profiles
+{
+    public class SchoolProfile : Profile
+    {
+        public SchoolProfile()
+        {
+            //Source => Target
+            CreateMap<School, SchoolReadDto>();
+            CreateMap<SchoolCreateDto, School>();
+            CreateMap<SchoolUpdateDto, School>();
+            CreateMap<School, SchoolUpdateDto>();
+        }
+    }
+}
