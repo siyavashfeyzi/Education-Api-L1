ec05a6ff05181423aeb676d18696ce541160e6ac
Siyavash <feyzisiyavash@gmail.com>
2021-02-12 23:20:39 +0330

    added services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Education_Api_L1
 {
@@ -26,7 +27,7 @@ namespace Education_Api_L1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
