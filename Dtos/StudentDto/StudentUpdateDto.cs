1443330f0cd9bc39fda8496b878c9a5075386a20
Siyavash <feyzisiyavash@gmail.com>
2021-02-12 22:42:25 +0330

    Create and Init StudentUpdateDto

---
+++ Dtos/StudentDto/StudentUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Dtos.StudentDto
+{
+    public class StudentUpdateDto
+    {
+        [Required]
+        [MaxLength(300)]
+        public string name { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string family { get; set; }
+
+        [MaxLength(11)]
+        [MinLength(11)]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        public string Year_of_Birth { get; set; }
+    }
+}
