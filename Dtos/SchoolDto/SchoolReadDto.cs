ccccfc321d11bed78ac902540b9d7f84847a2945
Siyavash <feyzisiyavash@gmail.com>
2021-02-12 22:47:35 +0330

    Create & Init SchoolReadDto

---
+++ Dtos/SchoolDto/SchoolReadDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Dtos.SchoolDto
+{
+    public class SchoolReadDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Level { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+    }
+}
