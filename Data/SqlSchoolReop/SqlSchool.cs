edf92c0a11997d547a8c9471471a989bc7caa38d
Siyavash <feyzisiyavash@gmail.com>
2021-02-13 09:59:23 +0330

    Create And Init SqlSchool

---
+++ Data/SqlSchoolReop/SqlSchool.cs
@@ -0,0 +1,63 @@
+﻿using Education_Api_L1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Data.SqlSchoolReop
+{
+    public class SqlSchool : ISchoolRepo
+    {
+        private readonly Education_Api_L1_Context _context;
+
+        public SqlSchool(Education_Api_L1_Context context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<School> GetAllSchools()
+        {
+            return _context.Schools.ToList();
+        }
+
+
+        public School GetById(int id)
+        {
+            return _context.Schools.FirstOrDefault(p => p.Id == id);
+        }
+
+
+        public void CreateSchool(School sch)
+        {
+            if (sch == null)
+            {
+                throw new ArgumentException(nameof(sch));
+            }
+
+            _context.Schools.Add(sch);
+        }
+
+
+        public void UpdateSchool(School sch)
+        {
+           
+        }
+
+
+        public void DeleteSchool(School sch)
+        {
+            if (sch == null)
+            {
+                throw new ArgumentException(nameof(sch));
+            }
+
+            _context.Schools.Remove(sch);
+        }
+
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
