8c0e30f27a4d02011c18a04802e4f7152e21ba45
Siyavash <feyzisiyavash@gmail.com>
2021-02-13 09:19:14 +0330

    Create and init IstudentRepo.cs

---
+++ Data/SqlStudent/IStudentRepo.cs
@@ -0,0 +1,19 @@
+﻿using Education_Api_L1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Data
+{
+    public interface IStudentRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Student> GetAllStudents();
+        Student GetById(int id);
+        void CreateStudent(Student std);
+        void UpdateStudent(Student std);
+        void DeleteStudent(Student std);
+
+    }
+}
