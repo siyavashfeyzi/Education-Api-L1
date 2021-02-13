ae6656a00c457080ed5bbff1294648dbcb2f217c
Siyavash <feyzisiyavash@gmail.com>
2021-02-13 09:12:07 +0330

    Create and Init Db Context

---
+++ Data/Education_Api_L1_Context.cs
@@ -0,0 +1,19 @@
+﻿using Education_Api_L1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Data
+{
+    public class Education_Api_L1_Context : DbContext
+    {
+        public Education_Api_L1_Context(DbContextOptions<Education_Api_L1_Context>opt) : base (opt)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+    }
+}
