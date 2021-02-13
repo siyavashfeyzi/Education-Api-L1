4996e687059d7429529f7448de414e8ecc29ead3
Siyavash <feyzisiyavash@gmail.com>
2021-02-13 09:44:14 +0330

    Create and init SqlSchoolRepo

---
+++ Data/SqlSchoolReop/ISchoolRepo.cs
@@ -0,0 +1,18 @@
+﻿using Education_Api_L1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Education_Api_L1.Data.SqlSchoolReop
+{
+    public interface ISchoolRepo
+    {
+        bool SaveChanges();
+        IEnumerable<School> GetAllSchools();
+        School GetById(int id);
+        void CreateSchool(School std);
+        void UpdateSchool(School std);
+        void DeleteSchool(School std);
+    }
+}
